11d80837e8cf2b594cb9069b64187b450537f1db
niccolo <niccolofato13@gmail.com>
2021-02-23 11:41:58 +0100

    Create posta.py

---
+++ project2/posta.py
@@ -0,0 +1,16 @@
+#Program to send emails
+import smtplib
+#We access e-mail
+print("\t\tSign in")
+my_email = str(input("Insert your email:"))
+my_password = str(input("Insert your password:"))
+print()
+oggetto = input("Inserisci l'oggetto della mail: ")
+messaggio = input("Ora puoi inserire il messaggio che vuoi inviare: ")
+contenuto = oggetto + messaggio
+print("Sto effettuando la connessione col Server...")
+server = smtplib.SMTP("smtp.gmail.com", 587) # questa configurazione funziona per gmail
+server.starttls() # email criptata
+server.login("niccolofato13@gmail.com", "Nick200312")
+server.sendmail("niccolofato13@gmail.com", "niccolofato14@gmail.com", contenuto)
+server.close()
